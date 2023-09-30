811455adfda0c031786820cd23419a99e3512ae6
Ruslan Voronov <voronov.rv.99@yandex.ru>
2023-09-30 16:03:59 +0300

    init: create login component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,24 @@
 export default function Login() {
-  return <h1>Login component</h1>;
+  return (
+    <section className="form">
+      <div className='form__container'>
+        <h2 className="form__title">Войти в систему</h2>
+        <form className="form__form" >
+          <div>
+            <div className='form__area'>
+              <p className="form__text">E-mail</p>
+              <input className='form__input' />
+            </div>
+            <div className='form__area'>
+              <p className="form__text">Пароль</p>
+              <input className='form__input' />
+            </div>
+          </div>
+          <button className="form__submit">
+            Войти
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
