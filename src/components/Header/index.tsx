85bda09d015099d9b3d4597305c0f49eea1eb3f6
Ruslan Voronov <voronov.rv.99@yandex.ru>
2023-10-05 21:52:32 +0300

    feat: add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,21 @@
 import logo from '../../images/Logo.svg';
-import './Header.scss';
+import exit from '../../images/door_exit.svg';
+import style from './Header.module.scss';
 
 function Header() {
   return (
-    <header className="header">
-      <img src={logo.src} alt="Логотип" className="header__logo" />
+    <header className={style.header}>
+      <div className={style.header__container}>
+        <img src={logo.src} alt="Логотип" className={style.header__logo} />
+        <nav className={style.header__nav}>
+          <a className={`${style.header__navItem} ${style.header__navItem_active}`}>Прогноз</a>
+          <a className={style.header__navItem}>Качество прогноза</a>
+        </nav>
+      </div>
+      <div className={style.header__container}>
+        <p>Васнецова Мария</p>
+        <img src={exit.src} className={style.header__exit} />
+      </div>
     </header>
   );
 }
