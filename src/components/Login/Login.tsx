f7c561a2c02373aa9a2c866cc5a8f5112246476f
Andrey Roditelev <rodandr13@gmail.com>
2023-10-29 12:01:39 +0300

    feat: add formik for login page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,66 +1,110 @@
 'use client';
 
+import { Field, Formik } from 'formik';
 import style from './Login.module.scss';
 import styleButton from '../Button/Button.module.scss';
 import { useState } from 'react';
-import { useForm } from '../../hooks/useForm';
+
+interface LoginValues {
+  email: string,
+  password: string,
+}
 
 export default function Login() {
-
-  const { values, handleChange, isValid, errors } = useForm({});
-  const handleSubmit = (evt: any): void => {
-    evt.preventDefault();
-  };
-
   const [passwordVisibility, setPasswordVisibility] = useState(false);
-  function showPassword(): void {
+  const initialValues: LoginValues = { email: '', password: '' };
+
+  const togglePasswordVisibility = () => {
     setPasswordVisibility(!passwordVisibility);
-  }
+  };
 
   return (
     <section className={style.login}>
-      <div>
-        <h2 className={style.login__title}>Войти в систему</h2>
-        <form onSubmit={handleSubmit}>
+      <h2 className={style.login__title}>Войти в систему</h2>
+      <Formik
+        initialValues={initialValues}
+        validate={values => {
+          const errors = {};
+          const EMAIL_REGEXP = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-          {/* Email */}
-          <div >
-            <p className={style.login__text}>E-mail</p>
-            <input className={`${errors.email ? `${style.login__input} ${style.login__input_error}` : style.login__input}`}
-              value={values.email || ''} onChange={handleChange}
-              name='email' type='email'
-              required />
-            <span className={style.login__errorMessage} id="email-error">{errors.email}</span>
-          </div>
+          if (!values.email) {
+            errors.email = 'Укажите email.';
+          } else if (!EMAIL_REGEXP.test(values.email)) {
+            errors.email = 'Формат почты неправильный.';
+          }
 
-          {/* Пароль */}
-          <div>
-            <p className={style.login__text}>Пароль</p>
-            <div className={style.login__wrapper}>
-              <input className={`${errors.password ? `${style.login__input} ${style.login__input_error}` : style.login__input}`}
-                value={values.password || ''} onChange={handleChange}
-                name='password'
-                type={`${passwordVisibility ? 'text' : 'password'}`} required />
-              <button className={`${!passwordVisibility ? style.password__btn : `${style.password__btn} ${style.password__btn_active}`}`} onClick={showPassword}></button>
+          if (!values.password) {
+            errors.password = 'Укажите пароль.';
+          }
+
+          return errors;
+        }}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            console.log({ values, actions });
+            alert(JSON.stringify(values, null, 2));
+            actions.setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          isSubmitting,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            {/* Email */}
+            <div>
+              <p className={style.login__text}>E-mail</p>
+              <Field
+                type='email'
+                name='email'
+                value={values.email}
+                className={`${touched.email && errors.email ? `${style.login__input} ${style.login__input_error}` : style.login__input}`}
+              />
+              <span className={`${style.login__errorMessage} ${errors.email ? style.login__errorMessage_active : ''}`} id="email-error">{touched.email && errors.email}</span>
             </div>
-            <span className={style.login__errorMessage} id="password-error">{errors.password}</span>
-          </div>
 
-          {/* Чекбокс */}
-          <div>
-            <label className={style.login__checkbox}>
-              <input type="checkbox" className={style.checkbox_type_invisible} />
-              <span className={style.checkbox_type_visible} />
-              <p className={style.checkbox__text}>Запомнить меня</p>
-            </label>
-          </div>
+            {/* Пароль */}
+            <div>
+              <p className={style.login__text}>Пароль</p>
+              <div className={style.login__wrapper}>
+                <Field
+                  type={`${passwordVisibility ? 'text' : 'password'}`}
+                  name='password'
+                  value={values.password || ''}
+                  className={`${touched.password && errors.password ? `${style.login__input} ${style.login__input_error}` : style.login__input}`}
+                  />
+                <button
+                  type='button'
+                  className={`${!passwordVisibility ? style.password__btn : `${style.password__btn} ${style.password__btn_active}`}`}
+                  onClick={togglePasswordVisibility}
+                ></button>
+              </div>
+              <span className={style.login__errorMessage} id="password-error">{touched.password && errors.password}</span>
 
-          {/* Сабмит */}
-          <button className={`${isValid ? styleButton.button : `${styleButton.button} ${styleButton.button_invalid}`}`} type="submit" disabled={!isValid}>
-            Войти
-          </button>
-        </form>
-      </div>
+            </div>
+
+            {/* Чекбокс */}
+            <div>
+              <label className={style.login__checkbox}>
+                <input type="checkbox" className={style.checkbox_type_invisible}/>
+                <span className={style.checkbox_type_visible}/>
+                <p className={style.checkbox__text}>Запомнить меня</p>
+              </label>
+            </div>
+
+            {/* Сабмит */}
+            <button
+              //className={`${isValid ? styleButton.button : `${styleButton.button} ${styleButton.button_invalid}`}`}
+              type="submit" disabled={isSubmitting}>
+              Войти
+            </button>
+          </form>
+        )}
+      </Formik>
     </section>
   );
 }
